0e9fd2613c834604229b9f922b2c690ec95b79f7
Lanahv <alanavieira410@gmail.com>
2024-04-24 12:31:12 -0300

    upppp

--- script.js
+++ script.js
@@ -27,51 +27,26 @@ function filterSelection(c) {
   }
 }
 
-// Show filtered elements
-function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
-    }
-  }
-}
 
-// Hide elements that are not selected
-function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
+document.addEventListener("DOMContentLoaded", function() {
+  const prevButton = document.querySelector(".prev-button");
+  const nextButton = document.querySelector(".next-button");
+  const carousel = document.querySelector(".carousel");
 
-// Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
+  let currentIndex = 0;
+
+  nextButton.addEventListener("click", () => {
+      currentIndex = (currentIndex + 1) % carousel.children.length;
+      updateCarousel();
   });
-}
 
+  prevButton.addEventListener("click", () => {
+      currentIndex = (currentIndex - 1 + carousel.children.length) % carousel.children.length;
+      updateCarousel();
+  });
 
-let count = 0;
-
-function incrementCount() {
-  count++;
-  document.getElementById("count-display").textContent = count;
-}
-
-
-
-
-
+  function updateCarousel() {
+      const slideWidth = carousel.children[0].offsetWidth;
+      carousel.style.transform = `translateX(${-currentIndex * slideWidth}px)`;
+  }
+});
