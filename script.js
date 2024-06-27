800c99d426a057679681c72078069dd4a5f1b45e
Lanahv <alanavieira410@gmail.com>
2024-06-27 11:37:39 -0300

    UPPPP
    deixei o form funcional

--- script.js
+++ script.js
@@ -84,3 +84,19 @@ btns.forEach((btn, i) => {
     sliderNav(i);
     });
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const form = document.querySelector("form");
+
+  form.addEventListener("submit", function(event) {
+      // Exemplo de validação simples
+      const nome = document.getElementById("nome").value;
+      const email = document.getElementById("email").value;
+
+      if (nome === "" || email === "") {
+          alert("Por favor, preencha todos os campos.");
+          event.preventDefault();
+      }
+  });
+});
