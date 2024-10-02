735e4b2ce61a13e9e32efe2e0b101031754431fd
Lanahv <alanavieira410@gmail.com>
2024-10-02 12:32:29 -0300

    uppp
    ad navbar

--- script.js
+++ script.js
@@ -31,3 +31,17 @@ btns.forEach((btn, i) => {
     });
 });
 
+
+
+
+
+
+
+function toggleNavbar() {
+    var navbar = document.getElementById("myNavbar");
+    if (navbar.className === "navbar") {
+        navbar.className += " responsive";
+    } else {
+        navbar.className = "navbar";
+    }
+}
