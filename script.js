c9e0faed69d701dc4a5a0c5892eac0a8433ebd35
ALANA LEMOS VIEIRA <alanavieira410@gmail.com>
2024-10-16 12:18:21 -0300

    uuup

--- script.js
+++ script.js
@@ -40,4 +40,21 @@ menuBtn.addEventListener("click", () => {
     navigation.classList.toggle("show"); // Alterna a classe show
   });
   
+//CARROSSEL LANÇAMENTO
+let currentSlide = 0;
+
+function moveSlide(direction) {
+    const slides = document.querySelectorAll('.slider img');
+    const totalSlides = slides.length;
+
+    // Atualiza a posição atual
+    currentSlide = (currentSlide + direction + totalSlides) % totalSlides;
+
+    // Calcula o deslocamento
+    const offset = -currentSlide * 25; // cada imagem ocupa 25%
+    document.querySelector('.slider-container').style.transform = `translateX(${offset}%)`;
+}
+
+// Para autoplay
+setInterval(() => moveSlide(1), 10000000); // muda a cada 3 segundos
 
